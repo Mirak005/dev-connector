5bf896cab3487a230e59d7207e9c44e28a869a80
Karim Gharbi <Mirak005>
2020-04-18 16:41:16 +0100

    prepare for deplyment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -10,13 +11,20 @@ connectDB();
 
 app.use(express.json({ extented: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
-
 //Routes
 app.use("/users", require("./routes/api/users"));
 app.use("/auth", require("./routes/api/auth"));
 app.use("/profile", require("./routes/api/profile"));
 app.use("/posts", require("./routes/api/posts"));
+
+//Serve Static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //Starting the Server
 const PORT = process.env.PORT || 5000;
